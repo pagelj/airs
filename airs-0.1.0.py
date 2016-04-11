487f360844c5d140ca3df94e41c1afdfce1570f4
Janis Pagel <pageljs@pfeifente.ims.uni-stuttgart.de>
2016-04-11 15:55:46 +0200

    Provided skeletons for basic modules

---
+++ airs-0.1.0.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2.7
+
+# coding=utf-8
+
+"""
+Awesome Information Retrieval System (AIRS) - Main File
+
+Contributors: Prajit Dhar, Janis Pagel
+University of Stuttgart
+Institute for Natural Language Processing
+Summer Term 16
+04/11/2016
+
+v 0.1.0
+
+"""
+
+#################################################
+######################### Import ################
+#################################################
+
+import token
+import term
+import document
+import postingslist
+import dictionary
+
+
+################################################
+##################### Classes ##################
+################################################
+
+
+class System(object):
+
+    # Class for managing and coordinating all the different components
+    
+    def __init__(self):
+
+        pass
+
+
+###############################################
+################# Main ########################
+###############################################
+
+def main():
+
+    pass
+
+if __name__=='__main__':
+
+    main()
---
+++ parsedoc.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+
+# coding=utf-8
+
+"""
+Awesome Information Retrieval System (AIRS) - Parse Document
+
+Contributors: Prajit Dhar, Janis Pagel
+University of Stuttgart
+Institute for Natural Language Processing
+Summer Term 16
+04/11/2016
+
+"""
+
+################################################
+##################### Classes ##################
+################################################
+
+class Parsedoc(object):
+
+    def __init__(self):
+
+        pass
+
+
+###########################################################
+####################### Testing ###########################
+###########################################################
+
+def main():
+
+    pass
+
+if __name__=='__main__':
+
+    main()
