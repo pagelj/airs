aab4904f00a3a4b90410244e299bb1d690a615a4
Janis Pagel <pagel@hyperion.saturn.de>
2016-06-10 10:42:47 +0200

    fixed bug when samplespace is less than five

--- modules/evaluation.py
+++ modules/evaluation.py
@@ -11,12 +11,12 @@ import random as rd
 
 
 class Evaluation(object):
-    
+
     def __init__(self,ranking):
         self.ranking=ranking.ranking
 
         describe=self.ranking.Cosine_Similarity.describe()
-        
+
         low=self.ranking[self.ranking.Cosine_Similarity<describe[4]]
 #        print "\nOld Low\n",low
 #        low.rename(columns={'Unnamed: 0':'Filename'}, inplace=True)
@@ -24,12 +24,22 @@ class Evaluation(object):
 #        mid.rename(columns={'Unnamed: 0':'Filename'}, inplace=True)
         high=self.ranking[self.ranking.Cosine_Similarity>=describe[6]]
 #        high.rename(columns={'Unnamed: 0':'Filename'}, inplace=True)
-        
-        samplelow=low.loc[rd.sample(list(low.index),5)]
-        samplemid=mid.loc[rd.sample(list(mid.index),5)]
-        samplehigh=high.loc[rd.sample(list(high.index),5)]
-        
-        
+
+
+        try:
+            samplelow=low.loc[rd.sample(list(low.index),5)]
+        except ValueError:
+            samplelow=low
+        try:
+            samplemid=mid.loc[rd.sample(list(mid.index),5)]
+        except ValueError:
+            samplemid=mid
+        try:
+            samplehigh=high.loc[rd.sample(list(high.index),5)]
+        except ValueError:
+            samplehigh=high
+
+
         files=[]
         filesamplelow=list(samplelow.index.values)
         filesamplemid=list(samplemid.index.values)
@@ -38,4 +48,3 @@ class Evaluation(object):
         files.extend(filesamplemid)
         files.extend(filesamplehigh)
         print files
-        
